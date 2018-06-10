b2ed63963647ecf1167aadbcee63e7e3314c1988
jeancsanchez <jeancs.patricio@gmail.com>
2018-06-10 19:01:05 -0300

    Migrando tela de cadastro de novo contato.

--- using-convalida/app/src/main/java/br/com/wellingtoncosta/comparative/ui/NewContactActivity.java
+++ using-convalida/app/src/main/java/br/com/wellingtoncosta/comparative/ui/NewContactActivity.java
@@ -1,18 +1,28 @@
 package br.com.wellingtoncosta.comparative.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import br.com.wellingtoncosta.comparative.R;
+import br.com.wellingtoncosta.comparative.domain.Contact;
+import br.com.wellingtoncosta.comparative.domain.User;
+import br.com.wellingtoncosta.comparative.util.RealmUtils;
+import br.com.wellingtoncosta.comparative.util.SharedPreferencesUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import convalida.annotations.EmailValidation;
+import convalida.annotations.OnValidationSuccess;
+import convalida.annotations.PatternValidation;
+import convalida.annotations.RequiredValidation;
+import convalida.annotations.ValidateOnClick;
+import io.realm.Realm;
 
 /**
  * @author Wellington Costa on 14/09/2017.
@@ -24,56 +34,44 @@ public class NewContactActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
-    //    @NotEmptyValidation(R.string.name_required)
     @BindView(R.id.nameLayout)
     TextInputLayout nameLayout;
 
+    @RequiredValidation(errorMessage = R.string.name_required)
     @BindView(R.id.nameField)
     EditText nameField;
 
-    //    @EmailValidation(R.string.invalid_email)
     @BindView(R.id.emailLayout)
     TextInputLayout emailLayout;
 
+    @EmailValidation(errorMessage = R.string.invalid_email)
     @BindView(R.id.emailField)
     EditText emailField;
 
     @BindView(R.id.phoneLayout)
     TextInputLayout phoneLayout;
 
-    //    @PatternValidation(errorMessage = R.string.phone_required, pattern = PHONE_PATTERN)
+    @PatternValidation(errorMessage = R.string.phone_required, pattern = PHONE_PATTERN)
     @BindView(R.id.phoneField)
     EditText phoneField;
+
+    @ValidateOnClick
+    @BindView(R.id.saveContactButton)
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
+
         ButterKnife.bind(this);
+        NewContactActivityFieldsValidation.init(this);
         setupToolbar();
     }
 
     @Override
     public void onBackPressed() {
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_new_contact, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_register_new_contact) {
-            registerNewContact();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void setupToolbar() {
@@ -87,36 +85,34 @@ public class NewContactActivity extends AppCompatActivity {
         });
     }
 
+    @OnValidationSuccess
     public void registerNewContact() {
         long start = System.currentTimeMillis();
-//        boolean fieldsAreValid = validator.validateFields();
         long end = System.currentTimeMillis();
 
         Log.d("Profiler", "Tempo de execucao = " + (end - start) + "ms");
 
-//        if(fieldsAreValid) {
-//            try {
-//                Realm realm = Realm.getDefaultInstance();
-//                int userLoggedId = SharedPreferencesUtils.getUserLogged(this);
-//                User userLogged = realm.where(User.class).equalTo("id", userLoggedId).findFirst();
-//
-//                if (userLogged != null) {
-//                    int nextContactId = RealmUtils.getNextValue(Contact.class);
-//
-//                    realm.beginTransaction();
-//                    Contact contact = realm.createObject(Contact.class, nextContactId);
-//                    contact.setName(nameField.getText().toString());
-//                    contact.setEmail(emailField.getText().toString());
-//                    contact.setPhone(phoneField.getText().toString());
-//                    userLogged.getContacts().add(contact);
-//                    realm.commitTransaction();
-//                }
-//
-//                finish();
-//            } catch (Exception ex) {
-//                Log.e("NewContactActivity", ex.getMessage());
-//                Snackbar.make(toolbar, "Não foi possível salvar o contato.", Snackbar.LENGTH_LONG).show();
-//            }
-//        }
+        try {
+            Realm realm = Realm.getDefaultInstance();
+            int userLoggedId = SharedPreferencesUtils.getUserLogged(this);
+            User userLogged = realm.where(User.class).equalTo("id", userLoggedId).findFirst();
+
+            if (userLogged != null) {
+                int nextContactId = RealmUtils.getNextValue(Contact.class);
+
+                realm.beginTransaction();
+                Contact contact = realm.createObject(Contact.class, nextContactId);
+                contact.setName(nameField.getText().toString());
+                contact.setEmail(emailField.getText().toString());
+                contact.setPhone(phoneField.getText().toString());
+                userLogged.getContacts().add(contact);
+                realm.commitTransaction();
+            }
+
+            finish();
+        } catch (Exception ex) {
+            Log.e("NewContactActivity", ex.getMessage());
+            Snackbar.make(toolbar, "Não foi possível salvar o contato.", Snackbar.LENGTH_LONG).show();
+        }
     }
 }
